cd90617f316f9abcb90ea12560c10d67245e47b8
Bruno Bruzzo <bruzzo.bruno@gmail.com>
2022-04-08 12:05:27 -0300

    Ejercicio 2

--- Laboratorio-1/main.cpp
+++ Laboratorio-1/main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 
-int f(int x){
-    return x+1;
+int f(int x, int y){
+    return x+y;
 }
 
 int main() {
-    std::cout << "El resultado es: " << f(10) << std::endl;
+    std::cout << "El resultado es: " << f(10,5) << std::endl;
     return 0;
 }
