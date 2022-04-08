c4a47cc55731d62e2a141e86e04358425669d450
Bruno Bruzzo <bruzzo.bruno@gmail.com>
2022-04-08 13:46:50 -0300

    Ejercicio 3

--- Laboratorio-1/main.cpp
+++ Laboratorio-1/main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 
 int f(int x, int y){
-    return x+y;
+    if (x > y) {
+        return x+y;
+    } else {
+        return x*y;
+    }
 }
 
 int main() {
-    std::cout << "El resultado es: " << f(10,5) << std::endl;
+    std::cout << "El resultado es: " << f(5,10) << std::endl;
     return 0;
 }
