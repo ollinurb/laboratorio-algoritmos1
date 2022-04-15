fcfc56579e76e74fee58f7e8b4af654fc5a00d35
Bruno Bruzzo <bruzzo.bruno@gmail.com>
2022-04-15 17:47:25 -0300

    Ejercicio 5 Problemas con la versión iterativa de Fibonacci

--- Laboratorio-1/main.cpp
+++ Laboratorio-1/main.cpp
@@ -19,11 +19,50 @@ bool esPrimo(int n) {
     else { return false; }
 }
 
-
-int main() {
-    int a;
+int funcionEsPrimo(int a) {
     std::cout << "Ingresar numero para verificar si es primo:" << std::endl;
     std::cin >> a;
     std::cout << (  esPrimo(a) ? "El número es primo." : "El número no es primo")<< std::endl;
+
+}
+
+int fibonacciRecursiva(int n) {
+    if (n == 0) {
+        return 0;
+    } else if (n == 1) {
+        return 1;
+    } else
+        return fibonacciRecursiva(n - 1) + fibonacciRecursiva(n - 2);
+    }
+
+int funcionFibonacciRecursiva (int n) {
+    std::cout << "Pedir número de Fibonacci" << std::endl;
+    std::cin >> n;
+    std::cout << fibonacciRecursiva(n) << std::endl;
+}
+// 0 1 1 2 3 5 8 13 21 34 55 ...
+int fibonacciIterativa (int n) {
+    int f0 = 0;
+    int f1 = 1;
+    int i = n;
+    int sum = 0;
+    while (i > 1) {
+        i = i - 1;
+        sum = f0 + f1;
+        f0 = f1;
+        f1 = sum;
+    }
+    return f1;
+}
+
+int funcionFibonacciIterativa(int n) {
+    std::cout << "Pedir numero de Fibonacci" << std::endl;
+    std::cin >> n;
+    std::cout << fibonacciIterativa(n) << std::endl;
+}
+
+int main() {
+    int a;
+    funcionFibonacciIterativa(a);
     return 0;
 }
